73817f2a62b4e2312f772e36983681ca77ef3765
aliizadjoo <Ali.zadjoo@gmail.com>
2025-10-20 08:02:33 +0330

    پروگرم اضافه کردن نظر سنجی

--- HWW16/Program.cs
+++ HWW16/Program.cs
@@ -7,6 +7,8 @@ using HWW16.Services;
 AppDbContext appDbContext = new AppDbContext();
 UserRepository userRepository = new UserRepository(appDbContext);
 UserService userService = new UserService(userRepository);
+SurveyRepository surveyRepository = new SurveyRepository(appDbContext);
+SurveyService surveyService = new SurveyService(surveyRepository);
 while (true)
 {
     if (LocalStorage.LoginUser == null)
@@ -34,17 +36,66 @@ while (true)
             case RoleEnum.Admin:
 
                 ShowMenuAdmin();
-                try 
+                try
                 {
                     int option = int.Parse(Console.ReadLine()!);
-                    switch (option) 
+                    switch (option)
                     {
                         case 1:
+                            Console.WriteLine("--- Add New Survey ---");
+                            Console.Write("Enter Survey Title: ");
+                            string surveyTitle = Console.ReadLine()!;
+                            var createSurveyDto = new CreateSurveyDto
+                            {
+                                Title = surveyTitle,
+                                Questions = new List<QuestionDto>()
+                            };
+                            while (true)
+                            {
+                                Console.WriteLine("\n--- Add New Question ---");
+                                Console.Write("Enter Question Text (or type 'done' to finish adding questions): ");
+                                string questionText = Console.ReadLine()!;
 
-                            break;
+                                if (questionText.ToLower() == "done")
+                                {
+                                    break;
+                                }
+
+                                var questionDto = new QuestionDto
+                                {
+                                    Text = questionText,
+                                    Options = new List<string>()
+                                };
+
+                                Console.WriteLine("Enter exactly 4 options for this question:");
+                                for (int i = 0; i < 4; i++)
+                                {
+                                    Console.Write($"Option {i + 1}: ");
+                                    string optionText = Console.ReadLine()!;
+                                    questionDto.Options.Add(optionText);
+                                }
+
+                                createSurveyDto.Questions.Add(questionDto);
+                            }
+                            try
+                            {
+                                surveyService.AddSurvey(createSurveyDto);
+                                Console.WriteLine("\nSurvey added successfully!");
+                            }
+                            catch (Exception ex)
+                            {
+
+                                Console.WriteLine($"Error adding survey: {ex.Message}");
+
+                            }
+                            Console.WriteLine("Press any key to return to the admin menu...");
+                            Console.ReadKey(); 
+                            Console.Clear();   
+                            break; 
+                            
                     }
                 }
-                catch (FormatException) 
+                catch (FormatException)
                 {
                     Console.WriteLine("invalid option please enter number");
                 }
@@ -52,13 +103,13 @@ while (true)
                 break;
 
             case RoleEnum.NormalUser:
-                
+
                 break;
         }
     }
 }
 
-void ShowMenuAdmin() 
+void ShowMenuAdmin()
 {
     Console.WriteLine("please enter option");
     Console.WriteLine("1.Add survey");
